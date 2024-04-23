80e0d3aedad3c82b9fce1eea2cf130c8790e662d
Steeven Bachet <steevy72500@gmail.com>
2024-04-23 16:56:30 +0200

    début de l'integration de housing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Housing from './pages/Housing';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Error from './pages/Error';
@@ -23,6 +24,7 @@ root.render(
         <Nav/>
         <Routes>
           <Route path="/" element={<Home houses={houses} />} />
+          <Route path="/housing" element={<Housing houses={houses} />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Error />} />
         </Routes>
--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,13 +1,34 @@
-import Nav from "../../components/Nav";
+import '../../styles/pages/housing/housing.css';
 
 function Housing() {
-    return (
-      <div>
-        <Nav/>
-        <p>C'est le Test pour Housing</p>
+  return (
+    <div className="housing">
+      <div className="housing__carrousel"></div>
+      <div className="housing__container">
+        <div className="housing__bloc-1">
+          <div className='housing__bloc-1__item'>
+            <h1 className="housing__bloc-1__title">LA LIEUX COSY</h1>
+            <p className="housing__bloc-1__txt">test de la localisation</p>
+          </div>
+          <p className="housing__bloc-1__name">LE PRENOM du proprio</p>
+
+        </div>
+        <div className="housing__bloc-2">
+            <div className='housing__bloc-2__item'>
+              <div>Cozy</div>
+              <div>Canal</div>
+              <div>Paris 10</div>
+            </div>
+            <div className='housing__bloc-2__star'>ETOILE</div>
+        </div>
+        <div className="housing__bloc-3">
+            <div>COLLAPSE 1</div>
+            <div>COLLAPSE 2</div>
+        </div>
+        
       </div>
-    );
-  }
-  
-  export default Housing;
-  
+    </div>
+  );
+}
+
+export default Housing;
