cb13edbd50f0ac67a856450c927ae92227754631
Steeven Bachet <steevy72500@gmail.com>
2024-04-23 16:18:16 +0200

    Collapse d'about fini (avec animation)

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -2,31 +2,28 @@ import { useState } from 'react';
 import '../../styles/components/collapse/collapse.css';
 import WhiteArrow from '../../assets/WhiteArrow.png';
 
-function Collapse() {
-  const collapseList = ['Fiabilité', 'Respect', 'Service', 'Sécurité'];
-  const [openIndex, setOpenIndex] = useState(null);
+function Collapse({ collapseList }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = (index) => {
-    setOpenIndex(openIndex === index ? null : index);
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
   };
 
-  /* <p> collapse txt => si je reçois une liste => <ul> sinon <p>*/
   return (
     <div className="collapse">
-      {collapseList.map((collapse, index) => (
-        <div key={index} className="collapse__item">
-          <p className="collapse__txt">{collapse}</p>
-          <button
-            className={`collapse__button ${openIndex === index ? 'collapse__open' : ''}`}
-            onClick={() => handleClick(index)}
-          >
-            <img className="collapse__image" src={WhiteArrow} alt="Flèche" />
-          </button>
-          <div className={`collapse__window ${openIndex === index ? 'active' : ''}`}>
-            Contenu de {collapse}
+      <div className="collapse__item">
+        <p className="collapse__txt">{collapseList[0]}</p>
+        <button className={`collapse__button ${isOpen ? 'active' : ''}`} onClick={toggleCollapse}>
+          <img
+            className={`collapse__image ${isOpen ? 'open' : ''}`}
+            src={WhiteArrow}
+            alt="Flèche"
+          />
+        </button>
+        <div className={`collapse__window ${isOpen ? 'active' : ''}`}>
+          <p className='collapse__window__txt'>{collapseList[1]}</p>
           </div>
-        </div>
-      ))}
+      </div>
     </div>
   );
 }
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,10 +3,26 @@ import Collapse from '../../components/Collapse';
 import '../../styles/pages/about/about.css';
 
 function About() {
+  const aboutFiabilite = [
+    "Fiabilité", "Les annonces postées sur Kasa garantissent un fiabilité totale, les photos sont conforme aux logement, et toutes les informations sont régulièrement vérifiées par nos équipes."
+  ]
+  const aboutRespect = [
+    "Respect", "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement descriminatoire ou de perturbation du voisinage entraînera un exclusion de notre plateforme"
+  ]
+  const aboutService = [
+    "Service", "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement descriminatoire ou de perturbation du voisinage entraînera un exclusion de notre plateforme"
+  ]
+  const aboutSecurite = [
+    "Sécurité", "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspondent au critères de sécurité établis pas nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes"
+  ]
+  
   return (
     <div className="about">
       <Banner />
-      <Collapse />
+      <Collapse collapseList={aboutFiabilite}/>
+      <Collapse collapseList={aboutRespect}/>
+      <Collapse collapseList={aboutService}/>
+      <Collapse collapseList={aboutSecurite}/>
     </div>
   );
 }
